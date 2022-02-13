52e2e124ae507c6824016cfa577d31ac9c6cd684
LekiaAnonim <lekiaprosper@gmail.com>
2022-02-13 16:57:20 +0100

    modify settings.py to add django_heroku setting at the bottom

--- koretgist/settings.py
+++ koretgist/settings.py
@@ -17,7 +17,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -258,3 +257,5 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+django_heroku.settings(locals())
